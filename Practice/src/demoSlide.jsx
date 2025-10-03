de77066c5d9e7772c40db1b414545dd5fe2abdf1
himanshu-dewangan-h3m <himanshu.dewangan@procuzy.com>
2025-10-03 21:57:48 +0530

    practice

---
+++ Practice/src/demoSlide.jsx
@@ -0,0 +1,29 @@
+const demoSlides = [
+  <div className="slide slide1" key="slide1">
+    <div className="slide-content">
+      <h2>Slide 1</h2>
+    </div>
+  </div>,
+  <div className="slide slide2" key="slide2">
+    <div className="slide-content">
+      <h2>Slide 2</h2>
+    </div>
+  </div>,
+  <div className="slide slide3" key="slide3">
+    <div className="slide-content">
+      <h2>Slide 3</h2>
+    </div>
+  </div>,
+  <div className="slide slide4" key="slide4">
+    <div className="slide-content">
+      <h2>Slide 4</h2>
+    </div>
+  </div>,
+  <div className="slide slide5" key="slide5">
+    <div className="slide-content">
+      <h2>Slide 5</h2>
+    </div>
+  </div>,
+];
+
+export default demoSlides;
